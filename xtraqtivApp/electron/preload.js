72d048fb47b8994d27155ecfdca5aaa1c0e5b217
Josh Way <22014636+JoshWay@users.noreply.github.com>
2025-06-04 13:49:33 -0400

    Fix Electron preload bridge

--- xtraqtivApp/electron/preload.js
+++ xtraqtivApp/electron/preload.js
@@ -1,1 +1,7 @@
- 
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  openExternal: (url) => ipcRenderer.invoke('open-external', url),
+  getAppInfo: () => ipcRenderer.invoke('get-app-info'),
+});
+
